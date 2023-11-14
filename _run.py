2775bed97a50f6a310b461a5f70519abfeaf9e57
Rufshod <alexander@11a.se>
2023-11-14 13:02:53 +0100

    Fixed global value for object_name that updates alongside anvil

--- _run.py
+++ _run.py
@@ -9,7 +9,6 @@ from multicamcomposepro.utils import Warehouse
 
 from ano import predict
 
-
 # Connect to Anvil server
 uplink_key = json.load(open("anvil_key.json"))["Server_Uplink_Key"]
 anvil.server.connect(uplink_key)
@@ -18,28 +17,44 @@ print("Connected to Anvil server")
 ## Flask server to serve images to Anvil
 app = Flask(__name__)
 object_name = "preview"
-path_to_images = os.path.join(os.getcwd(), "data_warehouse", "dataset", object_name, "train", "good")
+path_to_images = os.path.join(
+    os.getcwd(), "data_warehouse", "dataset", object_name, "train", "good"
+)
+
 
 # TODO path_to_distributions here
+
+
+# Function to set the object name from Anvil
+@anvil.server.callable
+def set_object_name(object_input_name: str = "object"):
+    global object_name
+    object_name = object_input_name
+    print("Object name set to:", object_name)
+    return object_name
 
 
 # Called when URL is loaded
 @app.route("/<angle>/<image>")
 def get_image(angle, image):
+    print("RUNNING GET_IMAGE")
     directory = os.path.join(
         os.getcwd(), "data_warehouse", "dataset", object_name, "train", "good", angle
     )
+    full_path = os.path.join(directory, image)
+    print("Full path to image:", full_path)
     # Add a cache-busting query parameter
     cache_buster = request.args.get("cb", int(time.time()))
 
     if os.path.isfile(os.path.join(directory, image)):
         response = send_from_directory(directory, image)
-        
+
         # Modify the cache control headers
         response.headers["Cache-Control"] = "no-store"
         return response
     else:
         return "File not found", 404
+
 
 @anvil.server.callable
 def get_distribution_list():
@@ -47,6 +62,7 @@ def get_distribution_list():
     folder_path = os.path.join(os.getcwd(), "data_warehouse", "distributions")
     folder_contents = os.listdir(folder_path)
     return folder_contents if folder_contents else "No distributions saved!"
+
 
 @anvil.server.callable
 def run_prediction(object_name):
@@ -56,8 +72,15 @@ def run_prediction(object_name):
     distributions_path = os.path.join(os.getcwd(), "data_warehouse", "distributions")
 
     # TODO update hardcoded cam_names with input strings from Anvil
-    predict(distributions_path, cam_name="cam_0_left", object_name=object_name, test_images=["/Users/helvetica/_master_anodet/anodet/data_warehouse/dataset/purple_duck/test/albinism/cam_0_left/001.png"], THRESH=13)
-
+    predict(
+        distributions_path,
+        cam_name="cam_0_left",
+        object_name=object_name,
+        test_images=[
+            "/Users/helvetica/_master_anodet/anodet/data_warehouse/dataset/purple_duck/test/albinism/cam_0_left/001.png"
+        ],
+        THRESH=13,
+    )
 
 
 @anvil.server.callable
@@ -124,24 +147,25 @@ def capture_initial_images():
 
 
 @anvil.server.callable
-def run_mccp():
-    """Runs the multicam compose pro program"""
-    print("mccp running")
+def capture_image(object_input_name: str = "object"):
+    """Function to capture a single image from the camera"""
+    print("Capturing image")
     # Get settings from json
     warehouse = Warehouse()
-    warehouse.build()
+    warehouse.build(object_name=object_input_name, anomalies=[])
 
     # Take picture
     camera_manager = CameraManager(
-        warehouse, train_images=1, test_anomaly_images=0, allow_user_input=False, overwrite_original = False
+        warehouse,
+        train_images=1,
+        test_anomaly_images=0,
+        allow_user_input=False,
+        overwrite_original=False,
     )
     camera_manager.run()
     print(warehouse)
-
-    
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
     anvil.server.wait_forever()
-
--- ano.py
+++ ano.py
@@ -1,31 +1,52 @@
 import os
+
 import cv2
+import matplotlib
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
-import matplotlib
-matplotlib.use('Agg')  # Set the backend to non-interactive one (Agg)
-import matplotlib.pyplot as plt
+
+matplotlib.use("Agg")  # Set the backend to non-interactive one (Agg)
 import sys
+
+import matplotlib.pyplot as plt
 
 # Get anodet by path
 current_dir = os.getcwd()
-target_dir = os.path.join(current_dir, '..', 'anodet')
+target_dir = os.path.join(current_dir, "..", "anodet")
 sys.path.append(target_dir)
 
-from anodet import Padim, AnodetDataset, to_batch, classification, visualization, standard_image_transform
+from anodet import (AnodetDataset, Padim, classification,
+                    standard_image_transform, to_batch, visualization)
+
 
 def get_dataloader(dataset_path, cam_name, object_name):
-    dataset = AnodetDataset(os.path.join(dataset_path, f"{object_name}/train/good/{cam_name}"))
+    dataset = AnodetDataset(
+        os.path.join(dataset_path, f"{object_name}/train/good/{cam_name}")
+    )
     dataloader = DataLoader(dataset, batch_size=32)
-    print(f"DataLoader created with {len(dataset)} images for {object_name}, camera {cam_name}.")
+    print(
+        f"DataLoader created with {len(dataset)} images for {object_name}, camera {cam_name}."
+    )
     return dataloader
+
 
 def model_fit(model, dataloader, distributions_path, cam_name, object_name):
     model.fit(dataloader)
-    torch.save(model.mean, os.path.join(distributions_path, f"{object_name}/{object_name}_{cam_name}_mean.pt"))
-    torch.save(model.cov_inv, os.path.join(distributions_path, f"{object_name}/{object_name}_{cam_name}_cov_inv.pt"))
+    torch.save(
+        model.mean,
+        os.path.join(
+            distributions_path, f"{object_name}/{object_name}_{cam_name}_mean.pt"
+        ),
+    )
+    torch.save(
+        model.cov_inv,
+        os.path.join(
+            distributions_path, f"{object_name}/{object_name}_{cam_name}_cov_inv.pt"
+        ),
+    )
     print(f"Parameters saved at {distributions_path}")
+
 
 def predict(distributions_path, cam_name, object_name, test_images, THRESH=13):
     print("ano - predict running")
@@ -33,10 +54,20 @@ def predict(distributions_path, cam_name, object_name, test_images, THRESH=13):
     images = [cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB) for path in test_images]
     batch = to_batch(images, standard_image_transform, torch.device("cpu"))
 
-    mean = torch.load(os.path.join(distributions_path, f"{object_name}/{object_name}_{cam_name}_mean.pt"))
-    cov_inv = torch.load(os.path.join(distributions_path, f"{object_name}/{object_name}_{cam_name}_cov_inv.pt"))
+    mean = torch.load(
+        os.path.join(
+            distributions_path, f"{object_name}/{object_name}_{cam_name}_mean.pt"
+        )
+    )
+    cov_inv = torch.load(
+        os.path.join(
+            distributions_path, f"{object_name}/{object_name}_{cam_name}_cov_inv.pt"
+        )
+    )
 
-    padim = Padim(backbone="resnet18", mean=mean, cov_inv=cov_inv, device=torch.device("cpu"))
+    padim = Padim(
+        backbone="resnet18", mean=mean, cov_inv=cov_inv, device=torch.device("cpu")
+    )
     image_scores, score_maps = padim.predict(batch)
 
     score_map_classifications = classification(score_maps, THRESH)
@@ -44,9 +75,13 @@ def predict(distributions_path, cam_name, object_name, test_images, THRESH=13):
 
     test_images = np.array(images).copy()
 
-    boundary_images = visualization.framed_boundary_images(test_images, score_map_classifications, image_classifications, padding=40)
+    boundary_images = visualization.framed_boundary_images(
+        test_images, score_map_classifications, image_classifications, padding=40
+    )
     heatmap_images = visualization.heatmap_images(test_images, score_maps, alpha=0.5)
-    highlighted_images = visualization.highlighted_images(images, score_map_classifications, color=(128, 0, 128))
+    highlighted_images = visualization.highlighted_images(
+        images, score_map_classifications, color=(128, 0, 128)
+    )
 
     for idx in range(len(images)):
         fig, axs = plt.subplots(1, 4, figsize=(12, 6))
@@ -55,13 +90,14 @@ def predict(distributions_path, cam_name, object_name, test_images, THRESH=13):
         axs[1].imshow(boundary_images[idx])
         axs[2].imshow(heatmap_images[idx])
         axs[3].imshow(highlighted_images[idx])
-        
+
         plt.savefig(f"data_warehouse/plots/plot_{idx}.png")
         plt.close()
 
     print("Saved figures.")
-        
+
     return image_classifications, image_scores, score_maps
+
 
 if __name__ == "__main__":
     print(sys.path)
